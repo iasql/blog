abe3df8c2d354fde8f7df8fc5834c40e33134224
Luis Fernando <lfdepombo@gmail.com>
2022-10-24 16:20:56 -0400

    add Hotjar + pass amp_device_id when using TheButton (#80)
    * pass amp_device_id when using TheButton
    
    * add hotjar
    
    * cover self-reference
    
    * fix typo

--- telemetry.js
+++ telemetry.js
@@ -1,14 +1,25 @@
-import amplitude from 'amplitude-js';
+import Amplitude from 'amplitude-js';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
-const amplitudeKey = 'c9d876059e7c9a83e44dcef855e77f48';
+const AMP_KEY = 'c9d876059e7c9a83e44dcef855e77f48';
+const HOTJAR_SITE_ID = 2927368;
+const HOTJAR_VERSION = 6;
 
 function addDeviceId() {
-  const deviceId = amplitude.getInstance().options.deviceId;
+  const deviceId = Amplitude.getInstance().options.deviceId;
+  // TheButton href^= matches on the beginning
+  document.querySelectorAll("[href^='https://app.iasql.com/#/button/']")
+    .forEach(n => n.href += `?amp_device_id=${deviceId}`);
+  // Dashboard
   document.querySelectorAll("[href='https://app.iasql.com']")
     .forEach(n => n.href = `https://app.iasql.com?amp_device_id=${deviceId}`);
+  // Docs
   document.querySelectorAll("[href='https://docs.iasql.com']")
     .forEach(n => n.href = `https://docs.iasql.com?amp_device_id=${deviceId}`);
+  // Blog
+  document.querySelectorAll("[href='https://blog.iasql.com']")
+  .forEach(n => n.href = `https://blog.iasql.com?amp_device_id=${deviceId}`);
+  // Landing page
   document.querySelectorAll("[href='https://iasql.com']")
     .forEach(n => n.href = `https://iasql.com?amp_device_id=${deviceId}`);
 }
@@ -17,7 +28,7 @@ export function onRouteDidUpdate({location, previousLocation}) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
   if (location.pathname !== previousLocation?.pathname) {
-    amplitude.getInstance().logEvent("BLOG", {
+    Amplitude.getInstance().logEvent("BLOG", {
       route: location.pathname,
     });
     addDeviceId();
@@ -27,8 +38,9 @@ export function onRouteDidUpdate({location, previousLocation}) {
 // browser only
 if (ExecutionEnvironment.canUseDOM) {
   // https://developers.amplitude.com/docs/advanced-settings#cross-domain-tracking-javascript
-  amplitude.getInstance().init(amplitudeKey, null, {includeReferrer: true, includeUtm: true, deviceIdFromUrlParam: true}, addDeviceId);
-  amplitude.getInstance().logEvent("BLOG", {
+  Amplitude.getInstance().init(AMP_KEY, null, {includeReferrer: true, includeUtm: true, deviceIdFromUrlParam: true}, addDeviceId);
+  Amplitude.getInstance().logEvent("BLOG", {
     route: document.location.pathname,
   });
+  Hotjar.init(HOTJAR_SITE_ID, HOTJAR_VERSION);
 }
